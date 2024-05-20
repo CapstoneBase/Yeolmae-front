84ae3a7fe7aca9417d46cd090da1a8ba8e98dbe5
ooylf <118517380+yoolf@users.noreply.github.com>
2024-05-20 09:23:51 +0900

    feat: 카드 수정

--- Yeolmae-Front/src/components/Common/PostCard.jsx
+++ Yeolmae-Front/src/components/Common/PostCard.jsx
@@ -7,11 +7,17 @@ const Card = styled.div`
   border: 2px solid rgb(227, 232, 237);
   border-radius: 5px;
   box-shadow: rgba(100, 100, 100, 0.1) 0px 0px 29px 0px;
+  display: flex;
+  flex-direction: column;
 `;
 
-// const CardThumbnail = styled.image``;
+const CardThumbnail = styled.img`
+  width: 80%;
+`;
 
-const CardContents = styled.div``;
+const CardContents = styled.div`
+  padding: 7px;
+`;
 
 const PostTitle = styled.h4`
   margin: 10px;
@@ -21,14 +27,16 @@ const PostAuthor = styled.h5`
   margin: 10px;
 `;
 
+const PostDate = styled(PostAuthor)``;
+
 function PostCard() {
   return (
     <Card>
-      {/* <CardThumbnail /> */}
+      <CardThumbnail src="\IMG_4276.PNG" />
       <CardContents>
         <PostTitle>게시글제목</PostTitle>
         <PostAuthor>작성자</PostAuthor>
-        <PostAuthor>2024.05.16</PostAuthor>
+        <PostDate>2024.05.16</PostDate>
       </CardContents>
     </Card>
   );
