d378138a780528744f1ca3019a0e1d457df85770
ooylf <118517380+yoolf@users.noreply.github.com>
2024-05-25 02:27:16 +0900

    style: themeprovider 추가

---
+++ Yeolmae-Front/src/style/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  font: ['NotoSans'],
+  fontSizes: {
+    small: '0.5rem',
+    medium: '1rem',
+    large: '1.5rem'
+  },
+
+  colors: {
+    gray: '#6C6C6C',
+    lightgray: '#F5F5F5',
+    purple: '#6558f5'
+  }
+};
+
+export default theme;
