d896f31b7012e952756c9e21ee9ff83ffd0aa08d
ooylf <118517380+yoolf@users.noreply.github.com>
2024-05-26 00:25:34 +0900

    feat: 그리드 레이아웃 추가

---
+++ Yeolmae-Front/src/components/Common/Grid.jsx
@@ -0,0 +1,11 @@
+'/node_modules/react-grid-layout/css/styles.css';
+'/node_modules/react-resizable/css/styles.css';
+
+import GridLayout from 'react-grid-layout';
+
+// const layout = [
+//   { i: 'key1', x: 0, y: 0, w: 1, h: 1 },
+//   { i: 'key2', x: 0, y: 0, w: 1, h: 1 },
+//   { i: 'key3', x: 0, y: 0, w: 1, h: 1 },
+//   { i: 'key4', x: 0, y: 0, w: 1, h: 1 }
+// ];
