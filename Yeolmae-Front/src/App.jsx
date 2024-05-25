28ccf0f4d079cff016f0f7dc879c767ac3f029ef
ooylf <118517380+yoolf@users.noreply.github.com>
2024-05-25 17:52:18 +0900

    refactor: lazy-suspense, memo, themeprovider 추가

--- Yeolmae-Front/src/App.jsx
+++ Yeolmae-Front/src/App.jsx
@@ -1,31 +1,34 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Suspense, lazy, memo } from 'react';
 import theme from './style/theme';
-import Main from './components/pages/MainPage/MainPage';
-import Signup from './components/pages/SignupPage/SignupPage';
-import Login from './components/pages/LoginPage/LoginPage';
-import Notfound from './components/pages/NotFound/NotFound';
 import Header from './components/Common/Header';
 
-function App() {
+const Main = lazy(() => import('./components/pages/MainPage/MainPage'));
+const Signup = lazy(() => import('./components/pages/MainPage/MainPage'));
+const Login = lazy(() => import('./components/pages/MainPage/MainPage'));
+const Notfound = lazy(() => import('./components/pages/MainPage/MainPage'));
+
+const App = memo(() => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <div>
-        {/* 페이지 경로 표시 */}
         <Link to="/">메인 홈</Link>
         <Link to="/signupPage">회원가입페이지</Link>
         <Link to="/loginPage">로그인페이지</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signupPage" element={<Signup />} />
-        <Route path="/loginPage" element={<Login />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <Suspense fallback={<div>Now Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signupPage" element={<Signup />} />
+          <Route path="/loginPage" element={<Login />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Suspense>
     </ThemeProvider>
   );
-}
+});
 
 export default App;
