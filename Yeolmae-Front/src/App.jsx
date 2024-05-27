91b915b90e90298859599f7c3c78649f5f0fd4e1
ooylf <60663156+ooylf@users.noreply.github.com>
2024-05-27 17:00:47 +0900

    chore: 폴더 구조 변경에 따른 import 경로 수정

--- Yeolmae-Front/src/App.jsx
+++ Yeolmae-Front/src/App.jsx
@@ -1,33 +1,36 @@
-import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Suspense, lazy, memo } from 'react';
+import GlobalStyle from './style/globalStyles';
 import theme from './style/theme';
 import Header from './components/Common/Header';
 
 const Main = lazy(() => import('./components/pages/MainPage/MainPage'));
-const Signup = lazy(() => import('./components/pages/MainPage/MainPage'));
-const Login = lazy(() => import('./components/pages/MainPage/MainPage'));
-const Notfound = lazy(() => import('./components/pages/MainPage/MainPage'));
+const Signup = lazy(() => import('./components/pages/SignupPage/SignupPage'));
+const Login = lazy(() => import('./components/pages/LoginPage/LoginPage'));
+const Notfound = lazy(() => import('./components/pages/NotFound/NotFound'));
 
 const App = memo(() => {
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <div>
-        <Link to="/">메인 홈</Link>
-        <Link to="/signupPage">회원가입페이지</Link>
-        <Link to="/loginPage">로그인페이지</Link>
-      </div>
-      <Suspense fallback={<div>Now Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/signupPage" element={<Signup />} />
-          <Route path="/loginPage" element={<Login />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
-      </Suspense>
-    </ThemeProvider>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <div>
+          <Link to="/">메인 홈</Link>
+          <Link to="/signupPage">회원가입페이지</Link>
+          <Link to="/loginPage">로그인페이지</Link>
+        </div>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/signupPage" element={<Signup />} />
+            <Route path="/loginPage" element={<Login />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </Suspense>
+      </ThemeProvider>
+    </>
   );
 });
 
