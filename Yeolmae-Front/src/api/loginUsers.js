a27daa15368f07f0d0e6283a196a766e3117e968
ooylf <118517380+yoolf@users.noreply.github.com>
2024-06-06 05:59:50 +0900

    test: 에러 메세지 출력 코드 추가

--- Yeolmae-Front/src/api/loginUsers.js
+++ Yeolmae-Front/src/api/loginUsers.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
-
-const API = '/api/vi/login';
+// import axiosAuthInstance from './axiosInstance';
 
 export const loginUser = async (id, password) => {
+  const API = '/api/v1/login';
   const body = { id, password };
+
+  console.log('request body: ', body);
   try {
-    const response = await axios.post(`${API}/login`, body);
+    console.log('loginUsers try');
+    const response = await axios.post(`${API}`, body);
     console.log(response);
     return response.data;
   } catch (error) {
-    console.log('error : ', error.response);
-    const { status, statusText } = error.response;
-    const message = error.response.data.message[0];
-    console.log(`${status} - ${statusText} - ${message}`);
+    if (error.response) {
+      console.log('error : ', error.response);
+      const { status, statusText, data } = error.response;
+      console.log(`${status} - ${statusText} - ${data.message}`);
+    } else if (error.request) {
+      console.log('No response received: ', error.request);
+    } else {
+      console.log('Error setting up request: ', error.message);
+    }
     throw error;
   }
 };
