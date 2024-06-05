3dbf509c98f072cb69c7de2534c199da8b24af5e
ooylf <118517380+yoolf@users.noreply.github.com>
2024-06-06 06:03:43 +0900

    refactor: login thunk 함수 추가

--- Yeolmae-Front/src/redux/modules/authSlice.js
+++ Yeolmae-Front/src/redux/modules/authSlice.js
@@ -1,56 +1,84 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
-import { reissueToken as reissueTokenAPI } from '../../api/reissueToken';
-
-// 토큰 만료 시간
-// export const TOKEN_TIME_OUT = 600 * 1000;
+import { loginUser } from '../../api/loginUsers';
+import { reissueToken } from '../../api/reissueToken';
 
 export const authSlice = createSlice({
-  name: 'authToken',
-  // 초기 상태(인가 여부, access token, refresh token)
+  name: 'auth',
+  // 초기 상태(인가 여부, access token, refresh token, error)
   initialState: {
+    // user: null,
     authenticated: false,
     accessToken: localStorage.getItem('accessToken') || null,
-    refreshToken: localStorage.getItem('refreshToken') || null
+    refreshToken: localStorage.getItem('refreshToken') || null,
+    error: null
   },
   // 리듀서
   reducers: {
+    // SET_USER: (state, action) => {
+    //   state.user = action.payload;
+    // },
     SET_TOKEN: (state, action) => {
       state.authenticated = true;
       state.accessToken = action.payload.accessToken;
       state.refreshToken = action.payload.refreshToken;
     },
     DELETE_TOKEN: (state) => {
-      state.authenticated = false;
       state.accessToken = null;
       state.refreshToken = null;
+    },
+    SET_ERROR: (state, action) => {
+      state.error = action.payload;
     }
   }
 });
 
 // 액션 타입
-export const { SET_TOKEN, DELETE_TOKEN } = authSlice.actions;
+export const { SET_TOKEN, DELETE_TOKEN, SET_ERROR } = authSlice.actions;
 
-export default authSlice.reducer;
+export const loginThunk = (id, password) => async (dispatch) => {
+  try {
+    const data = await loginUser(id, password);
+    // dispatch(SET_USER(data.user));
+    dispatch(
+      SET_TOKEN({ accessToken: data.data.accessToken, refreshToken: data.data.refreshToken })
+    );
+    console.log('SET_TOKEN dispatch');
+    localStorage.setItem('accessToken', data.data.accessToken);
+    localStorage.setItem('refreshToken', data.data.refreshToken);
+    console.log('토큰 저장');
+    console.log(data.data.refreshToken);
+  } catch (error) {
+    dispatch(SET_ERROR(error.message));
+  }
+};
 
 // reissue thunk 함수
-export const reissueToken = () => async (dispatch, getState) => {
+export const reissueTokenThunk = () => async (dispatch, getState) => {
   const state = getState();
-  const { refreshToken } = state.reissueToken;
+  const { refreshToken } = state.auth;
 
   try {
     // reissueTokenAPI 함수로 서버에 토큰 재발급 요청
-    const data = await reissueTokenAPI(refreshToken);
-    // 토큰 재발급 성공 후 새로운 토큰 로컬에 저장
-    const { accessToken, refreshToken: newRefreshToken } = data;
-    localStorage.setItem('accessToken', accessToken);
+    const data = await reissueToken(refreshToken);
     // SET_TOKEN 액션 디스패치, 리덕스 상태 업데이트
-    localStorage.setItem('refreshToken', refreshToken);
-    dispatch(SET_TOKEN({ accessToken, refreshToken: newRefreshToken }));
+    console.log('reissue API req');
+    dispatch(
+      SET_TOKEN({ accessToken: data.data.accessToken, refreshToken: data.data.refreshToken })
+    );
+    // 토큰 재발급 성공 후 새로운 토큰 로컬에 저장
+    console.log('SET_TOKEN dispatch');
+    localStorage.setItem('accessToken', data.data.accessToken);
+    localStorage.setItem('refreshToken', data.data.refreshToken);
+    console.log('토큰 저장');
+    console.log('새 refreshToken: ', data.data.refreshToken);
   } catch (error) {
     // 토큰 재발급 실패
     console.error('토큰 재발급 실패', error);
+    dispatch(SET_ERROR(error.message));
     // DELETE_TOKEN 액션 디스패치해 인증상태 초기화
     dispatch(DELETE_TOKEN());
   }
 };
+
+export default authSlice.reducer;
