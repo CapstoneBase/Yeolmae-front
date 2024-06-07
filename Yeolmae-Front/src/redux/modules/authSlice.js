8e5d69b3c233cef7f874f1b585d80c73bb5b2b3e
ooylf <118517380+yoolf@users.noreply.github.com>
2024-06-07 18:08:13 +0900

    refactor: 로그인, 토큰 재발급 미들웨어 분리

--- Yeolmae-Front/src/redux/modules/authSlice.js
+++ Yeolmae-Front/src/redux/modules/authSlice.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
-import { loginUser } from '../../api/loginUsers';
-import { reissueToken } from '../../api/reissueToken';
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -35,48 +33,5 @@ export const authSlice = createSlice({
 
 // 액션 타입
 export const { SET_TOKEN, DELETE_TOKEN, SET_ERROR } = authSlice.actions;
-
-export const loginThunk = (id, password) => async (dispatch) => {
-  try {
-    const data = await loginUser(id, password);
-    // dispatch(SET_USER(data.user));
-    dispatch(SET_TOKEN({ accessToken: data.accessToken, refreshToken: data.refreshToken }));
-    console.log('SET_TOKEN dispatch');
-    localStorage.setItem('accessToken', data.accessToken);
-    localStorage.setItem('refreshToken', data.refreshToken);
-    console.log('토큰 저장');
-    console.log(data.refreshToken);
-  } catch (error) {
-    dispatch(SET_ERROR(error.message));
-  }
-};
-
-// reissue thunk 함수
-export const reissueTokenThunk = () => async (dispatch, getState) => {
-  const state = getState();
-  const { refreshToken } = state.auth;
-
-  try {
-    // reissueTokenAPI 함수로 서버에 토큰 재발급 요청
-    const data = await reissueToken(refreshToken);
-    // SET_TOKEN 액션 디스패치, 리덕스 상태 업데이트
-    console.log('reissue API req');
-    dispatch(
-      SET_TOKEN({ accessToken: data.data.accessToken, refreshToken: data.data.refreshToken })
-    );
-    // 토큰 재발급 성공 후 새로운 토큰 로컬에 저장
-    console.log('SET_TOKEN dispatch');
-    localStorage.setItem('accessToken', data.accessToken);
-    localStorage.setItem('refreshToken', data.refreshToken);
-    console.log('토큰 저장');
-    console.log('새 refreshToken: ', data.refreshToken);
-  } catch (error) {
-    // 토큰 재발급 실패
-    console.error('토큰 재발급 실패', error);
-    dispatch(SET_ERROR(error.message));
-    // DELETE_TOKEN 액션 디스패치해 인증상태 초기화
-    dispatch(DELETE_TOKEN());
-  }
-};
 
 export default authSlice.reducer;
