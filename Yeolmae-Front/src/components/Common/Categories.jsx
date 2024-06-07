db54d23c4eee293470ba7dbddef5ad3ec95eb361
sooya99 <limsy1371@gmail.com>
2024-06-07 21:11:05 +0900

    refactor: categories 배열 추가 (주석 처리)

--- Yeolmae-Front/src/components/Common/Categories.jsx
+++ Yeolmae-Front/src/components/Common/Categories.jsx
@@ -1,6 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import axios from 'axios';
+/*
+{
+  "results": [
+      {
+        "parentCategoryID": 1,
+        "parentcategoryName": "인문학",
+        "categories": [
+          {"categoryID": 1, "categoryName": "인문학일반"},
+          {"categoryID": 2, "categoryName": "역사학"},
+          {"categoryID": 3, "categoryName": "철학"},
+          {"categoryID": 4, "categoryName": "종교학/신학"},
+          {"categoryID": 5, "categoryName": "언어학"},
+          {"categoryID": 6, "categoryName": "문학"}
+        ]
+      },
+      {
+        "parentCategoryID": 2,
+        "parentcategoryName": "사회과학",
+        "categories": [
+          {"categoryID": 1, "categoryName": "사회과학일반"},
+          {"categoryID": 2, "categoryName": "정치외교학"},
+          {"categoryID": 3, "categoryName": "경제학"},
+          {"categoryID": 4, "categoryName": "경영학"},
+          {"categoryID": 5, "categoryName": "무역학"},
+          {"categoryID": 6, "categoryName": "사회학"}
+        ]
+      }
+  ]
+} */
+
 // import axios from '../hooks/useAxios';
 
 /* const Categories = ({ onSelect, category }) => {
