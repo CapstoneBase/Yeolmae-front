7aa79bccc13ec78c0fd2336d5d9b51d38e2a8dbc
ooylf <118517380+yoolf@users.noreply.github.com>
2024-06-13 05:04:10 +0900

    rename: postListPage 이름 변경

---
+++ Yeolmae-Front/src/components/pages/PostListPage/PostListPage.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import Button from '../../Common/Button';
+import PostCard from '../../Common/PostCard';
+import PageGrid from '../../Common/PageGrid';
+import Categories from '../../Common/DropdownItem';
+import category from '../../Common/category.json';
+// import CategoryDropdown from '../../Common/DropdownItem';
+// import Categories, { SubCategory } from '../../Common/DropdownItem';
+
+function PostList() {
+  return (
+    <>
+      {/* <CategoryDropdown /> */}
+      <Categories data={category} />
+      {/* <SubCategory data={category} /> */}
+      <PageGrid />
+    </>
+  );
+}
+
+export default PostList;
