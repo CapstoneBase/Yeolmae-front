516d504aa3d24ebdd24ef57fb9f3bfdd115dd029
ooylf <60663156+ooylf@users.noreply.github.com>
2024-06-13 18:13:15 +0900

    refactor: category 오타 수정

--- Yeolmae-Front/src/api/getPostList.js
+++ Yeolmae-Front/src/api/getPostList.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
-export const getPostList = async (parCategory, category, page, size) => {
-  const API = `api/v1/posts?&parentCategory=${parCategory}&category=${catergory}&page=${page}&size=${size}`;
+export const getPostList = async ({ parCategory, category, page, size }) => {
+  const API = `api/v1/posts?&parentCategory=${parCategory}&category=${category}&page=${page}&size=${size}`;
   // const API = 'api/v1/posts?&page=0&size=12';
   const body = { parCategory, category, page, size };
   console.log('request body: ', body);
