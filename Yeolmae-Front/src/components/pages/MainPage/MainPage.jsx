b953c99029463b08e9372780f0f3a20dc147962d
ooylf <60663156+ooylf@users.noreply.github.com>
2024-06-13 14:42:35 +0900

    feat: 메인 화면 구성

--- Yeolmae-Front/src/components/pages/MainPage/MainPage.jsx
+++ Yeolmae-Front/src/components/pages/MainPage/MainPage.jsx
@@ -1,11 +1,37 @@
-import styled, { css } from 'styled-components';
+import { Navigate, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '../../Common/Button';
 
+const ImageBox = styled.div`
+  display: grid;
+  margin: auto;
+  place-items: center;
+  gap: 50px;
+
+  img {
+    // margin: auto;
+  }
+`;
+const ButtonBox = styled.div`
+  display: grid;
+  width: 200px;
+  height: 30px;
+  margin: 0 200px;
+  gap: 20px;
+`;
 function Main() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('./postlistPage');
+  };
   return (
-    <div>
+    <ImageBox>
       <img src="/main_logo.PNG" alt="로고이미지" />
-      열매 메인 페이지 입니다.
-    </div>
+      <ButtonBox>
+        {/* <Button text="회원가입 하러 가기" onClick={navigate('/signupPage')} /> */}
+        <Button text="게시글 둘러보러 가기" onClick={handleClick} />
+      </ButtonBox>
+    </ImageBox>
   );
 }
 
