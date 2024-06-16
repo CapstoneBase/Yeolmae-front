d784176d0f5bb316650f814ba7be408ccfc3f55a
ooylf <118517380+yoolf@users.noreply.github.com>
2024-06-16 22:43:04 +0900

    refactor: 로그인 실패시 실행 알람 추가

--- Yeolmae-Front/src/redux/modules/loginThunk.js
+++ Yeolmae-Front/src/redux/modules/loginThunk.js
@@ -1,17 +1,19 @@
 import { loginUser } from '../../api/loginUser';
-import { SET_TOKEN, SET_ERROR } from './authSlice';
+import { SET_TOKEN, SET_ERROR, DELETE_TOKEN } from './authSlice';
 
 export const loginThunk = (id, password) => async (dispatch) => {
-    try {
-      const data = await loginUser(id, password);
-      // dispatch(SET_USER(data.user));
-      dispatch(SET_TOKEN({ accessToken: data.accessToken, refreshToken: data.refreshToken }));
-      console.log('SET_TOKEN dispatch');
-      localStorage.setItem('accessToken', data.accessToken);
-      localStorage.setItem('refreshToken', data.refreshToken);
-      console.log('토큰 저장');
-      console.log('로컬 스토리지에 저장된 refresh Token: ', data.refreshToken);
-    } catch (error) {
-      dispatch(SET_ERROR(error.message));
-    }
-  };
+  try {
+    const data = await loginUser(id, password);
+    // dispatch(SET_USER(data.user));
+    dispatch(SET_TOKEN({ accessToken: data.accessToken, refreshToken: data.refreshToken }));
+    console.log('SET_TOKEN dispatch');
+    localStorage.setItem('accessToken', data.accessToken);
+    localStorage.setItem('refreshToken', data.refreshToken);
+    console.log('토큰 저장');
+    console.log('로컬 스토리지에 저장된 refresh Token: ', data.refreshToken);
+  } catch (error) {
+    dispatch(SET_ERROR(error.message));
+    dispatch(DELETE_TOKEN());
+    console.log('로그인 api 컴포넌트 에러');
+  }
+};
