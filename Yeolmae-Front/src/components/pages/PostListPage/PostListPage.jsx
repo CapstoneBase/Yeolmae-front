20a2aa85b865bb63c35b65bfd6544dd5d4374770
ooylf <118517380+yoolf@users.noreply.github.com>
2024-09-01 22:19:25 +0900

    refactor: 카테고리 변경 리렌더링 오류수정(미완)

--- Yeolmae-Front/src/components/pages/PostListPage/PostListPage.jsx
+++ Yeolmae-Front/src/components/pages/PostListPage/PostListPage.jsx
@@ -15,19 +15,33 @@ const SelectBoxCol = styled.div`
 function PostList() {
   // 메인 페이지에서 선택한 카테고리 항목 상태를 받아온다
   const location = useLocation();
-  const cateInit = { ...location.state };
+  const sessionData = sessionStorage.getItem('category');
+  const cateData = JSON.parse(sessionData);
+  // const cateInit = { ...location.state };
 
   // 카테고리 초기 상태를 받아온 상태로 설정한다
-  const [input, setInput] = useState({
-    category: `${cateInit.cateId}`,
-    parentCategory: `${cateInit.parntCateId}`
+  const [curCategory, setCurCategory] = useState({
+    category: `${cateData.cateId}`,
+    parentCategory: `${cateData.parntCateId}`
   });
+  console.log('초기 카테고리: ', curCategory);
+
+  useEffect(() => {
+    sessionStorage.setItem(
+      'category',
+      JSON.stringify({
+        cateId: `${curCategory.cateId}`,
+        cateName: `${curCategory.cateName}`,
+        parntCateId: `${curCategory.parntCateId}`
+      })
+    );
+  }, [curCategory]);
 
   const [curPage, setCurPage] = useState(0);
   const [pageSize] = useState(8);
   // totalItems 실제 데이터에 따라 변경 필요
   const totalItems = 30;
-  // console.log(input);
+  // console.log(curCategory);
 
   // 페이지 이동 시 스크롤 위치 초기화
   useEffect(() => {
@@ -35,10 +49,29 @@ function PostList() {
   }, [curPage]);
 
   const handleCatChange = (e) => {
-    setInput({
-      ...input,
+    // const { name, value } = e.target;
+    //
+    // setCurCategory((prevCategory) => {
+    //   // category 변경시 parentCategory를 parntCateId로 변경
+    //   if (name === 'category') {
+    //     return {
+    //       ...prevCategory,
+    //       parentCategory: parntCateId,
+    //     };
+    //   }
+    //   return {
+    //     ...prevCategory,
+    //     [name]: value
+    //   };
+    // });
+
+    setCurCategory({
+      ...curCategory,
       [e.target.name]: e.target.value
     });
+    // parentCategory 변경 시 parntCateId와 parentCategory가 일치하는지 확인하는 기능 필요
+    // category 변경 시 parentCategory가 parntCateId로 변경되도록 하는 기능 필요
+    console.log(e.target.name, e.target.value);
     // 카테고리 변경시 첫번째 페이지로 이동
     setCurPage(0);
   };
@@ -49,9 +82,9 @@ function PostList() {
 
   console.log(
     '소분류: ',
-    input.category,
+    curCategory.category,
     '대분류: ',
-    input.parentCategory,
+    curCategory.parentCategory,
     '현재 페이지: ',
     curPage
   );
@@ -63,7 +96,7 @@ function PostList() {
           key="selParentCategory"
           name="parentCategory"
           onChange={handleCatChange}
-          value={input.parentCategory}
+          value={curCategory.parentCategory}
         >
           {Categories.map((item) =>
             item.parntCateId === '00' ? (
@@ -73,9 +106,14 @@ function PostList() {
             ) : null
           )}
         </Select>
-        <Select key="selCategory" name="category" onChange={handleCatChange} value={input.category}>
+        <Select
+          key="selCategory"
+          name="category"
+          onChange={handleCatChange}
+          value={curCategory.category}
+        >
           {Categories.map((item) =>
-            item.parntCateId === input.parentCategory ? (
+            item.parntCateId === curCategory.parentCategory ? (
               <option key={`selCategory${item.cateId}`} value={item.cateId}>
                 {item.cateName}
               </option>
@@ -84,8 +122,8 @@ function PostList() {
         </Select>
       </SelectBoxCol>
       <PageGrid
-        parCategory={input.parentCategory}
-        category={input.category}
+        parCategory={curCategory.parentCategory}
+        category={curCategory.category}
         page={curPage}
         size={pageSize}
       />
