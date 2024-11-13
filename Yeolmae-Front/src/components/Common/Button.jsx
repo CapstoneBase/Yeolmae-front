0b88ae31a41628e203c2d1882be8837827a52de8
ooylf <118517380+yoolf@users.noreply.github.com>
2024-11-14 01:40:34 +0900

    refactor: 버튼 컴포넌트 수정

--- Yeolmae-Front/src/components/Common/Button.jsx
+++ Yeolmae-Front/src/components/Common/Button.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  color: white;
-  background-color: #6558f5;
-  cursor: pointer;
-  border: none;
-  border-radius: 3px;
-  padding: 7px 25px;
-  font-family: NotoSans Regular;
-  font-size: 15px;
-  white-space: nowrap;
-`;
 
 function Button({ text, type, onClick }) {
   return (
-    <StyledButton type={type} onClick={onClick}>
+    <button type="button" className="btn btn-outline-primary" onClick={onClick}>
       {text}
-    </StyledButton>
+    </button>
   );
 }
 
