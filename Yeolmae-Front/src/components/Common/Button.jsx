7d644085a8ae71cbf15b9a11eb63c23822d9eb02
ooylf <118517380+yoolf@users.noreply.github.com>
2024-11-14 01:44:49 +0900

    comment 추가

--- Yeolmae-Front/src/components/Common/Button.jsx
+++ Yeolmae-Front/src/components/Common/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Button({ text, type, onClick }) {
   return (
+    // 클래스 이름 - 색상 변경
     <button type="button" className="btn btn-outline-primary" onClick={onClick}>
       {text}
     </button>
