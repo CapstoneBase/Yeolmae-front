7de7d9bb8ff9a53000bb6d17e13306db37db5152
Jihoo Jeon <3294005@dankook.ac.kr>
2024-11-14 19:01:23 +0900

    refactor: 창 크기 변경 시 정렬 방식 수정

--- Yeolmae-Front/src/components/Common/Header.jsx
+++ Yeolmae-Front/src/components/Common/Header.jsx
@@ -27,56 +27,57 @@ function Header() {
     // 클래스이름 수정 필요
     <header className="p-3 bg-light bg-opacity-75 border-bottom">
       <div className="container">
-        <div className="d-flex align-items-center justify-content-center justify-content-lg-start">
-          <a
-            href="/"
-            className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
-          >
-            <img src="/main_logo.PNG" alt="Logo" width="60" height="32" />
-          </a>
+        <div className="row">
+          <div className="d-flex align-items-center justify-content-between">
+            <a
+              href="/"
+              className="d-flex mb-2 mb-lg-0 align-items-center text-dark text-decoration-none"
+            >
+              <img src="/main_logo.PNG" alt="Logo" width="60" height="32" />
+            </a>
 
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li>
-              <a href="/postlistPage" className="nav-link px-2 text-dark">
-                졸업 작품
-              </a>
-            </li>
-            <li>
-              <a href="/postlistPage" className="nav-link px-2 text-dark">
-                개인 프로젝트
-              </a>
-            </li>
-            <li>
-              <a href="/postlistPage" className="nav-link px-2 text-dark">
-                대회 및 공모전
-              </a>
-            </li>
-            <li>
-              <a href="/portfolioPage" className="nav-link px-2 text-dark">
-                포트폴리오
-              </a>
-            </li>
-          </ul>
+            <ul className="nav col-auto me-lg-auto mb-2 mb-md-0 justify-content-start">
+              <li>
+                <a href="/postlistPage" className="nav-link px-2 text-dark">
+                  졸업 작품
+                </a>
+              </li>
+              <li>
+                <a href="/postlistPage" className="nav-link px-2 text-dark">
+                  개인 프로젝트
+                </a>
+              </li>
+              <li>
+                <a href="/postlistPage" className="nav-link px-2 text-dark">
+                  대회 및 공모전
+                </a>
+              </li>
+              <li>
+                <a href="/portfolioPage" className="nav-link px-2 text-dark">
+                  포트폴리오
+                </a>
+              </li>
+            </ul>
 
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          {/* <ul className="nav"> */}
-            <li>
-              <a href="/myPage" className="nav-link px-2 text-dark">
-                마이페이지
-              </a>
-            </li>
-            <li>
-              {authenticated ? (
-                <a href="/" className="nav-link px-2 text-dark" onClick={handleClick}>
-                  로그아웃
+            <ul className="nav col-auto ms-lg-auto mb-2 mb-md-0 justify-content-end">
+              <li>
+                <a href="/myPage" className="nav-link px-2 text-dark">
+                  마이페이지
                 </a>
-              ) : (
-                <a href="/loginPage" className="nav-link px-2 text-dark" onClick={handleClick}>
-                  로그인
-                </a>
-              )}
-            </li>
-          </ul>
+              </li>
+              <li>
+                {authenticated ? (
+                  <a href="/" className="nav-link px-2 text-dark" onClick={handleClick}>
+                    로그아웃
+                  </a>
+                ) : (
+                  <a href="/loginPage" className="nav-link px-2 text-dark" onClick={handleClick}>
+                    로그인
+                  </a>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
