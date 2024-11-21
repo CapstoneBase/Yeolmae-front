a42cc4c10ccdb90e6652903a6b9ff88fa019a0f3
ooylf <118517380+yoolf@users.noreply.github.com>
2024-11-21 17:13:48 +0900

    comment : 주석 삭제

--- Yeolmae-Front/src/components/Common/Button.jsx
+++ Yeolmae-Front/src/components/Common/Button.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Button({ text, onClick }) {
   return (
-    // 클래스 이름 - 색상 변경
     <button className="btn btn-primary w-100 my-3 py-2" type="submit" onClick={onClick}>
       {text}
     </button>
