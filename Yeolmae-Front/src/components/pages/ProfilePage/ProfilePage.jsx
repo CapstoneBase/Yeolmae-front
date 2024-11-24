6757998b5e3335141a3f2c82e5964dc3034170f9
ooylf <118517380+yoolf@users.noreply.github.com>
2024-11-24 17:19:20 +0900

    add: 프로필 페이지 추가

---
+++ Yeolmae-Front/src/components/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,17 @@
+function ProfilePage() {
+  return (
+    <>
+      <div className="row mx-5 my-4 px-4">
+        <h2>마이프로필</h2>
+      </div>
+      <section>
+        <div>프로필 영역</div>
+      </section>
+      <section>
+        <div>작성글 영역</div>
+      </section>
+    </>
+  );
+}
+
+export default ProfilePage;
