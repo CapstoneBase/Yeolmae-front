1eff84a126ca44c7be66c68a62113f15f44f9b6d
Jihoo Jeon <3294005@dankook.ac.kr>
2024-11-28 06:43:08 +0900

    add: 포트폴리오 편집 페이지

--- Yeolmae-Front/src/components/pages/Portfolio/PortfolioEditPage.jsx
+++ Yeolmae-Front/src/components/pages/Portfolio/PortfolioEditPage.jsx
@@ -1,5 +1,169 @@
+import { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import createApiRequest from '../../../api/queryStrReq';
+import Button from '../../Common/Button';
+
+function CustomInput({ label, ...props }) {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{label}</label>
+      <input className="form-control" {...props} />
+    </div>
+  );
+}
+
 function PortfolioEditPage(params) {
-  return <div>포트폴리오 편집 페이지</div>;
+  const initialUserInfo = {
+    name: '김열매',
+    birthDate: '2000-10-10',
+    email: 'kym@example.com',
+    phone: '010-1234-5678',
+    address: '서울시 강남구',
+    introduction: '김열매라고 합니다'
+  };
+  const accessToken = localStorage.getItem('accessToken');
+
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [awardDate, setAwardDate] = useState(new Date());
+
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    birthDate: '',
+    contact: [],
+    selfIntroduction: null,
+    experiences: [
+      {
+        title: '',
+        startDate: '',
+        endDate: '',
+        description: ''
+      }
+    ],
+    education: [
+      {
+        title: '',
+        startDate: '',
+        endDate: '',
+        description: ''
+      }
+    ],
+    skill: [],
+    certifications: [],
+    awards: [
+      {
+        title: '',
+        description: '',
+        awardDate: ''
+      }
+    ],
+    contestPosts: [],
+    graduationProjectPosts: [],
+    otherProjectPosts: []
+  });
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const response = await createApiRequest('/portfolio', null, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+      setUserInfo(response);
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+    // console.log(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createApiRequest('/portfolio', null, {
+      method: 'PUT',
+      data: userInfo,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    console.log('Updated user info:', userInfo);
+  };
+
+  return (
+    <>
+      <div className="row mx-5 my-4 px-4">
+        <h2>포트폴리오 수정하기</h2>
+      </div>
+      <CustomInput
+        label="이름"
+        type="text"
+        name="name"
+        value={userInfo.name}
+        onChange={handleChange}
+      />
+      <label className="form-label me-3 mb-3">생년월일</label>
+      <DatePicker
+        label="생년월일"
+        className="form-control"
+        selected={birthDate}
+        onChange={(date) => setBirthDate(date)}
+        dateFormat="yyyy-MM-dd"
+        name="birthDate"
+        value={userInfo.birthDate}
+      />
+      {/* <label className="form-label">생년월일</label>
+      <DatePicker
+        selected={birthDate}
+        onChange={(date) => setBirthDate(date)}
+        dateFormat="yyyy-MM-dd"
+        className="form-control mb-3"
+        value={userInfo.birthDate}
+      /> */}
+      <div className="mb-3">
+        <label className="form-label">자기소개</label>
+        <textarea
+          className="form-control"
+          type="text"
+          name="selfIntroduction"
+          value={userInfo.selfIntroduction}
+          onChange={handleChange}
+        />
+      </div>
+      <CustomInput
+        label="연락처"
+        type="text"
+        name="phone"
+        value={userInfo.phone}
+        onChange={handleChange}
+      />
+
+      <div className="my-4">
+        <label className="form-label">경력</label>
+        {/* <DatePicker
+          className="form-control"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          name="startDate"
+          dateFormat="yyyy-MM-dd"
+          value={userInfo.experiences.startDate}
+        /> */}
+        <input type="text" aria-label="First name" className="form-control mb-1" />
+        <input type="text" aria-label="First name" className="form-control mb-1" />
+        <input type="text" aria-label="Last name" className="form-control" />
+      </div>
+      <Button onClick={handleSubmit} text="수정하기" />
+    </>
+  );
 }
 
 export default PortfolioEditPage;
--- Yeolmae-Front/src/components/pages/Portfolio/PortfolioPage.jsx
+++ Yeolmae-Front/src/components/pages/Portfolio/PortfolioPage.jsx
@@ -1,12 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PorfolioInfo from './PortfolioInfo';
 
+const API_INFO = '/api/v1/portfolio';
 const API_CONTEST_POSTS = '/api/v1/portfolio/contest-posts';
 const API_GRADUATION_PROJECT_POSTS = '/api/v1/portfolio/graduation-project-posts';
 const API_OTHER_PROJECT_POSTS = '/api/v1/portfolio/other-project-posts';
 
+function InfoRow({ label, value }) {
+  return (
+    <div className="row m-1 justify-items-center">
+      <div className="col-2 mx-2">
+        <h6>{label}</h6>
+      </div>
+      <div className="col-4">
+        <div>{value}</div>
+      </div>
+    </div>
+  );
+}
+function ExRow({ label, value }) {
+  return (
+    <div className="row m-1 justify-items-center">
+      <div className="col-2 mx-2">
+        <h6>{label}</h6>
+      </div>
+      <div className="col-4">
+        <div>{value}</div>
+      </div>
+    </div>
+  );
+}
+
+function InfoCard({ title, startDate, endDate, description }) {
+  return (
+    <div className="card m-3">
+      <div className="card-body">
+        <div className="row">
+          <div className="col-4">
+            <h5 className="card-title">{title}</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">
+              {startDate}-{endDate}
+            </h6>
+          </div>
+          <div className="col-8">
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function PortfolioPage() {
+  const [info, setInfo] = useState([]);
   const [contestPosts, setContestPosts] = useState([]);
   const [graduationProjectPosts, setGraduationProjectPosts] = useState([]);
   const [otherProjectPosts, setOtherProjectPosts] = useState([]);
@@ -20,28 +68,36 @@ function PortfolioPage() {
           throw new Error('로그인이 필요합니다.');
         }
 
-        const [contestResponse, graduationResponse, otherResponse] = await Promise.all([
-          axios.get(API_CONTEST_POSTS, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          }),
-          axios.get(API_GRADUATION_PROJECT_POSTS, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          }),
-          axios.get(API_OTHER_PROJECT_POSTS, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          })
-        ]);
+        const [infoResponse, contestResponse, graduationResponse, otherResponse] =
+          await Promise.all([
+            axios.get(API_INFO, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`
+              }
+            }),
+            axios.get(API_CONTEST_POSTS, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`
+              }
+            }),
+            axios.get(API_GRADUATION_PROJECT_POSTS, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`
+              }
+            }),
+            axios.get(API_OTHER_PROJECT_POSTS, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`
+              }
+            })
+          ]);
 
+        setInfo(infoResponse.data);
         setContestPosts(contestResponse.data);
         setGraduationProjectPosts(graduationResponse.data);
         setOtherProjectPosts(otherResponse.data);
         setLoading(false);
+        console.log(infoResponse.data);
       } catch (error) {
         console.error('Error fetching posts:', error);
       }
@@ -56,8 +112,49 @@ function PortfolioPage() {
 
   return (
     <div className="container mt-5">
-      <h1>내 포트폴리오</h1>
+      <div className="row mx-5 my-4 px-4">
+        <h2>나의 포트폴리오</h2>
+      </div>
+      <div className="row mx-5 px-4 justify-content-between">
+        <div className="col-6 d-flex justify-content-start align-items-center">
+          <h4>나의 정보</h4>
+        </div>
+        <div className="col-10 mx-2 my-4 p-2 justify-items-center">
+          <InfoRow label="생년월일" value={info.birthDate} />
+          <InfoRow label={info.contact[0].type} value={info.contact[0].value} />
+          <InfoRow label="학교" value={info.school} />
+          <InfoRow label="학과" value={info.major} />
+          <InfoRow label="자기소개" value={info.selfIntroduction} />
 
+          {info.experiences.map((index) => (
+            <InfoCard
+              key={index.id}
+              title={index.title}
+              startDate={index.startDate}
+              endDate={index.endDate}
+              description={index.description}
+            />
+          ))}
+          {info.education.map((index) => (
+            <InfoCard
+              key={index.id}
+              title={index.title}
+              startDate={index.startDate}
+              endDate={index.endDate}
+              description={index.description}
+            />
+          ))}
+          {info.awards.map((index) => (
+            <InfoCard
+              key={index.id}
+              title={index.title}
+              startDate={index.startDate}
+              endDate={index.endDate}
+              description={index.description}
+            />
+          ))}
+        </div>
+      </div>
       <section className="mb-4">
         <h2>공모전 게시물</h2>
         <div className="row">
