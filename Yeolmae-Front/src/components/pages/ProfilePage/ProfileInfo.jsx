9b341e83206f7ae4c3b58d3a352e1be009038dc2
ooylf <118517380+yoolf@users.noreply.github.com>
2024-12-01 14:26:01 +0900

    style: 프로필 자기소개 라벨 추가

--- Yeolmae-Front/src/components/pages/ProfilePage/ProfileInfo.jsx
+++ Yeolmae-Front/src/components/pages/ProfilePage/ProfileInfo.jsx
@@ -57,7 +57,7 @@ function ProfileInfo({ memberId, setUserInfo }) {
       <InfoRow label="이메일" value={info.email} />
       <InfoRow label="학교" value={info.school} />
       <InfoRow label="학과" value={info.major} />
-      <InfoRow value={info.introduction} />
+      <InfoRow label="자기소개" value={info.introduction} />
     </div>
   );
 }
